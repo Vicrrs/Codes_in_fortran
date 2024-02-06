a92d55b576105017158362d3a3bfe450a3f47627
Vicrrs <enricofermi2019@gmail.com>
2024-02-05 20:57:00 -0400

    Variaveis em Fortran

---
+++ 02-variaveis-e-tipos-de-dados/variaveis.f90
@@ -0,0 +1,7 @@
+program variaveis
+    implicit none
+    integer :: inteiro = 10
+    real :: realNumber = 5.5
+    print *, "Valor inteiro: ", inteiro
+    print *, "Numero real: ", realNumber
+end program variaveis
