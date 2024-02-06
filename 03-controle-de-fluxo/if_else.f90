0385ce31e5fe0ec079ac9b3955a2952bbc518c89
Vicrrs <enricofermi2019@gmail.com>
2024-02-05 21:25:37 -0400

    Controle de Fluxo em Fortran

--- 03-controle-de-fluxo/if_else.f90
+++ 03-controle-de-fluxo/if_else.f90
@@ -1,10 +1,10 @@
 program if_else
-    implicit none
-    integer :: numero = 5
+  implicit none
+  integer :: numero = 5
 
-    if (numero > 0) then
-      print *, "Número é positivo."
-    else
-      print *, "Número é negativo ou zero."
-    end if
+  if (numero > 0) then
+    print *, "Número é positivo."
+  else
+    print *, "Número é negativo ou zero."
+  end if
 end program if_else
